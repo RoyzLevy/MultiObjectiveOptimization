6e86fc730994801c20c61978c00591ba843df10a
Levy <roy.levy@intel.com>
2021-01-19 20:21:51 +0200

    4 nodes try of TSP

--- opti.py
+++ opti.py
@@ -11,37 +11,37 @@ from pymoo.visualization.scatter import Scatter
 
 class travel_pro(Problem):
 
-    def __init__(self, t0=5, t1=7, t2=3, d0=100, d1=50, d2=140):
-        xl = 0* np.ones(3)
-        xu = np.ones(3)
-        super().__init__(n_var=3, n_obj=2, n_constr=6, xl=xl, xu=xu, type_var=np.int)
-
-        self.t0=t0
-        self.t1=t1
-        self.t2=t2
-        self.d0=d0
-        self.d1=d1
-        self.d2=d2
+    def __init__(self, times, distances):
+        xl = 0* np.ones(len(times))
+        xu = np.ones(len(times))
+        super().__init__(n_var=len(times), n_obj=2, n_constr=8, xl=xl, xu=xu, type_var=np.int)
+        self.times=times
+        self.distances=distances
 
     def _evaluate(self, X, out, *args, **kwargs):
         # objectives
-        f_time = X[:,0]*self.t0 + X[:,1]*self.t1 + X[:,2]*self.t2
-        f_distance = X[:,0]*self.d0 + X[:,1]*self.d1 + X[:,2]*self.d2
+        f_time = X[:,0]*self.times[0] + X[:,1]*self.times[1] + X[:,2]*self.times[2] + X[:,3]*self.times[3] + X[:,4]*self.times[4] + X[:,5]*self.times[5]
+        f_distance = X[:,0]*self.distances[0] + X[:,1]*self.distances[1] + X[:,2]*self.distances[2] + X[:,3]*self.distances[3] + X[:,4]*self.distances[4] + X[:,5]*self.distances[5]
         out["F"] = np.column_stack([f_time, f_distance])
-        # out["F"] = f_distance
+        # out["F"] = f_time
 
         # constraints
-        g0 = X[:,0] + X[:,2] - 2 # x0 + x2 = 2
-        g1 = X[:,1] + X[:,2] - 2 # x1 + x2 = 2
-        g2 = X[:,1] + X[:,0] - 2 # xq + x0 = 2
+        g0 = X[:,0] + X[:,1] + X[:,4] - 2
+        g1 = X[:,0] + X[:,3] + X[:,5] - 2
+        g2 = X[:,5] + X[:,4] + X[:,2] - 2
+        g3 = X[:,1] + X[:,3] + X[:,2] - 2
 
-        g3 = - X[:,0] - X[:,2] + 2 # x0 + x2 = 2
-        g4 = - X[:,1] - X[:,2] + 2 # x1 + x2 = 2
-        g5 = - X[:,1] - X[:,0] + 2 # xq + x0 = 2
+        g4 = -X[:,0] - X[:,1] - X[:,4] + 2
+        g5 = -X[:,0] - X[:,3] - X[:,5] + 2 
+        g6 = -X[:,5] - X[:,4] - X[:,2] + 2
+        g7 = -X[:,1] - X[:,3] - X[:,2] + 2
 
-        out["G"] = anp.column_stack([g0, g1, g2, g3, g4, g5])
+        out["G"] = anp.column_stack([g0, g1, g2, g3, g4, g5, g6, g7])
 
-problem = travel_pro()
+times       = [5,7,3,10,2,1]
+distances   = [100,50,140,120,80,90]
+
+problem = travel_pro(times, distances)
 
 ################### Algorithm chosen:
 
@@ -63,7 +63,7 @@ algorithm = NSGA2(pop_size=10)
 
 res = minimize(problem,
                algorithm,
-               ('n_gen', 100),
+               ('n_gen', 50),
                verbose=True)
 
 ################### Print solutions:
@@ -72,8 +72,8 @@ print("Best solution found: %s" % res.X)
 print("Function value: %s" % res.F)
 print("Constraint violation: %s" % res.CV)
 
-plot = Scatter()
-plot.add(problem.pareto_front(), plot_type="line", color="black", alpha=0.7)
-plot.add(res.F, color="red")
-plot.show()
+# plot = Scatter()
+# plot.add(problem.pareto_front(), plot_type="line", color="black", alpha=0.7)
+# plot.add(res.F, color="red")
+# plot.show()
 # Scatter().add(res.F).show()
