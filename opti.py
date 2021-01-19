7ee0f85487a7c3a028d38aae16e5c1d398204200
Levy <roy.levy@intel.com>
2021-01-19 19:26:25 +0200

    2 objective NSGA II working

--- opti.py
+++ opti.py
@@ -7,6 +7,8 @@ from pymoo.optimize import minimize
 import autograd.numpy as anp
 from pymoo.visualization.scatter import Scatter
 
+################### Problem:
+
 class travel_pro(Problem):
 
     def __init__(self, t0=5, t1=7, t2=3, d0=100, d1=50, d2=140):
@@ -14,7 +16,7 @@ class travel_pro(Problem):
         xu = np.ones(3)
         super().__init__(n_var=3, n_obj=2, n_constr=6, xl=xl, xu=xu, type_var=np.int)
 
-        self.t0=t0 
+        self.t0=t0
         self.t1=t1
         self.t2=t2
         self.d0=d0
@@ -23,8 +25,8 @@ class travel_pro(Problem):
 
     def _evaluate(self, X, out, *args, **kwargs):
         # objectives
-        f_time = 0.8 * (X[:,0]*self.t0 + X[:,1]*self.t1 + X[:,2]*self.t2)
-        f_distance = 0.2* (X[:,0]*self.d0 + X[:,1]*self.d1 + X[:,2]*self.d2
+        f_time = X[:,0]*self.t0 + X[:,1]*self.t1 + X[:,2]*self.t2
+        f_distance = X[:,0]*self.d0 + X[:,1]*self.d1 + X[:,2]*self.d2
         out["F"] = np.column_stack([f_time, f_distance])
         # out["F"] = f_distance
 
@@ -33,29 +35,38 @@ class travel_pro(Problem):
         g1 = X[:,1] + X[:,2] - 2 # x1 + x2 = 2
         g2 = X[:,1] + X[:,0] - 2 # xq + x0 = 2
 
-        g3 = -X[:,0] - X[:,2] + 2 # x0 + x2 = 2
-        g4 = -X[:,1] - X[:,2] + 2 # x1 + x2 = 2
-        g5 = -X[:,1] - X[:,0] + 2 # xq + x0 = 2
+        g3 = - X[:,0] - X[:,2] + 2 # x0 + x2 = 2
+        g4 = - X[:,1] - X[:,2] + 2 # x1 + x2 = 2
+        g5 = - X[:,1] - X[:,0] + 2 # xq + x0 = 2
 
         out["G"] = anp.column_stack([g0, g1, g2, g3, g4, g5])
 
-problem =travel_pro()
+problem = travel_pro()
 
-# algorithm = NSGA2(pop_size=10)
+################### Algorithm chosen:
 
-algorithm = get_algorithm("ga",
-                       pop_size=20,
-                       sampling=get_sampling("int_random"),
-                       crossover=get_crossover("int_sbx", prob=1.0, eta=3.0),
-                       mutation=get_mutation("int_pm", eta=3.0),
-                       eliminate_duplicates=True,
-                       )
+algorithm = NSGA2(pop_size=10)
+# algorithm = NSGA2(pop_size=100,
+#                   sampling=get_sampling("bin_random"),
+#                   crossover=get_crossover("bin_two_point"),
+#                   mutation=get_mutation("bin_bitflip"),
+#                   eliminate_duplicates=True)
+
+# algorithm = GA(
+#     pop_size=200,
+#     sampling=get_sampling("bin_random"),
+#     crossover=get_crossover("bin_hux"),
+#     mutation=get_mutation("bin_bitflip"),
+#     eliminate_duplicates=True)
+
+################### Solve problem:
 
 res = minimize(problem,
                algorithm,
-               ('n_gen', 30),
-               seed=1,
+               ('n_gen', 100),
                verbose=True)
+
+################### Print solutions:
 
 print("Best solution found: %s" % res.X)
 print("Function value: %s" % res.F)
@@ -65,4 +76,4 @@ plot = Scatter()
 plot.add(problem.pareto_front(), plot_type="line", color="black", alpha=0.7)
 plot.add(res.F, color="red")
 plot.show()
-Scatter().add(res.F).show()
+# Scatter().add(res.F).show()
